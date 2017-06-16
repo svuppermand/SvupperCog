cc1b1e7c4826b603ec0cfe0c9cc87fee16efba28
svuppermand <twister.light@yahoo.fr>
2017-06-16 06:58:14 +0200

    Update summon.py

--- summon/summon.py
+++ summon/summon.py
@@ -8,13 +8,23 @@ class summon:
         self.bot = bot
 
     @commands.command()
-    async def summon(self,voice_channel):
-        """This does stuff!"""
+    async def summon(self, channel, author, voice_channel):
+       """
+        Usage:
+            {command_prefix}summon
+        Call the bot to the summoner's voice channel.
+        """
 
-        #simple command
-        self.bot.join_voice_channel(voice_channel)
-        self.move_voice_client(voice_channel)
-        await self.bot.say("channel joined !")
+        if not author.voice_channel:
+            raise exceptions.CommandError('You are not in a voice channel!')
+
+        voice_client = self.the_voice_clients.get(channel.server.id, None)
+        if voice_client and voice_client.channel.server == author.voice_channel.server:
+            await self.move_voice_client(author.voice_channel)
+            await self.bot.say("channel joined !")
+            return
+
+        
 
 def setup(bot):
     bot.add_cog(summon(bot))
